41b009377290acfc81987e45d6433aa6bc37162a
merraculous <bella434@gmail.com>
2025-02-05 17:02:38 -0800

    Change layout from hardcode to dbc. Update graph format

--- social_justice/shooting_visuatlisations.py
+++ social_justice/shooting_visuatlisations.py
@@ -1,146 +1,124 @@
-from pathlib import Path
-import csv
-
 from dash import Dash, dcc, html, Input, Output, State
+import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import plotly.express as px
 import pandas as pd
-import re
 
-import utils.dash_reusable_components as drc
-
-# read data and remove last 3 rows
+# read data and process
 df = pd.read_csv('data/mapping_police_violence_snapshot_061920.csv')
 
 df = df[df['State'] != 'Data from 1/1/2013 - 12/31/2019']
 df.dropna(how='all', inplace=True)
+df.fillna(0, inplace=True)
 
-stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
-app = Dash(__name__, external_stylesheets=stylesheets)
+# Define dropdown options for people killed
+ppl_options = [
+    {"value": "# Black people killed", "label": "Black people"},
+    {"value": "# Hispanic people killed", "label": "Hispanic people"},
+    {"value": "# Native American people killed", "label": "Native Americans"},
+    {"value": "# Asian people killed", "label": "Asian people"},
+    {"value": "# Pacific Islanders killed", "label": "Pacific Islanders"},
+    {"value": "# White people killed", "label": "White people"},
+    {"value": "# Unknown Race people killed", "label": "Unknown Race"},
+]
 
-app.layout = html.Div(
+app = Dash(__name__, external_stylesheets=[dbc.themes.MORPH])
+
+app.layout = dbc.Container(
     children=[
-        # .container class is fixed, .container.scalable is scalable
-        html.Div(
-            className="banner",
-            children=[
-                # Change App Name here
+        # Banner Section
+        dbc.Row(
+            dbc.Col(
                 html.Div(
-                    className="container scalable",
+                    className="banner",
                     children=[
                         # Change App Name here
-                        html.H2(
-                            id="banner-title",
+                        html.Div(
+                            className="container scalable",
                             children=[
-                                html.A(
-                                    "Police Shootings Snapshot 1/1/2013 - 12/31/2019",
-                                    style={
-                                        "text-decoration": "none",
-                                        "color": "inherit",
-                                    },
-                                )
+                                html.H2(
+                                    id="banner-title",
+                                    children=[
+                                        html.A(
+                                            "Police Shootings Snapshot 1/1/2013 - 12/31/2019",
+                                            style={
+                                                "text-decoration": "none",
+                                                "color": "inherit",
+                                            },
+                                        )
+                                    ],
+                                ),
                             ],
-                        ),
+                        )
                     ],
-                )
-            ],
+                ),
+                width={"size": 12, "order": 1, "offset": 0},
+            )
         ),
-        html.Div(
-            id="body",
-            className="container scalable",
+
+        # Main Body Section
+        dbc.Row(
             children=[
-                html.Div(
-                    id="app-container",
-                    # className="row",
+                # Left Column with Dropdowns
+                dbc.Col(
                     children=[
-                        html.Div(
-                            # className="three columns",
-                            id="left-column",
+                        dbc.Card(
+                            id="first-card",
                             children=[
-                                drc.Card(
-                                    id="first-card",
-                                    children=[
-                                        drc.NamedDropdown(
-                                            name="Select State",
+                                dbc.CardBody(
+                                    [
+                                        dbc.Label("Select State"),
+                                        dcc.Dropdown(
                                             id="state-dropdown",
-                                            multi=True,
-                                            options=df["State"].unique(),
+                                            options=[{"label": state, "value": state} for state in
+                                                     df["State"].unique()],
                                             value=['Alabama'],
-                                            clearable=False,
-                                            searchable=False,
-                                        ),
-                                    ],
-                                ),
-                                drc.Card(
-                                    id="last-card",
-                                    children=[
-                                        drc.NamedDropdown(
-                                            name="Select Data",
-                                            id="ppl-dropdown",
                                             multi=True,
-                                            options=[
-                                                # Options to be which data to list.
-                                                {
-                                                    "value": "# Black people killed",
-                                                    "label": "Black people"
-                                                },
-                                                {
-                                                    "value": "# Hispanic people killed",
-                                                    "label": "Hispanic people"
-                                                },
-                                                {
-                                                    "value": "# Native American people killed",
-                                                    "label": "Native Americans"
-                                                },
-                                                {
-                                                    "value": "# Asian people killed",
-                                                    "label": "Asian people"
-                                                },
-                                                {
-                                                    "value": "# Pacific Islanders killed",
-                                                    "label": "Pacific Islanders"
-                                                },
-                                                {
-                                                    "value": "# White people killed",
-                                                    "label": "White people"
-                                                },
-                                                {
-                                                    "value": "# Unknown Race people killed",
-                                                    "label": "Unknown Race"
-                                                },
-                                            ],
-                                            value=['# Black people killed'],
                                             clearable=False,
                                             searchable=False,
                                         ),
-                                    ],
+                                    ]
+
                                 ),
                             ],
                         ),
-                        html.Div(
-                            id="div-graphs",
-                            children=dcc.Graph(
-                                id="line-chart",
-                                figure={},
-                                className="row"
-                            ),
+                        dbc.Card(
+                            id="last-card",
+                            children=[
+                                dbc.CardBody(
+                                    [
+                                        dbc.Label("Select Data"),
+                                        dcc.Dropdown(
+                                            id="ppl-dropdown",
+                                            options=ppl_options,
+                                            value=['# Black people killed'],
+                                            multi=True,
+                                            clearable=False,
+                                            searchable=False,
+                                        ),
+                                    ]
+
+                                ),
+                            ],
                         ),
-                        # html.Div(
-                        #     html.A(
-                        #         id="my-link",
-                        #         children="Click here to Visit Twitter",
-                        #         href="https://twitter.com/explore",
-                        #         target="_blank",
-                        #     ),
-                        #     className="two columns",
-                        # ),
                     ],
-                )
+                    width=3,  # Adjust the column width as necessary
+                ),
+
+                # Graph Section
+                dbc.Col(
+                    dcc.Graph(
+                        id="line-chart",
+                        figure={},
+                        className="row",
+                    ),
+                    width=8,# Adjust the column width as necessary
+                ),
             ],
         ),
-    ]
+    ],
+    fluid=True,  # Makes the container responsive
 )
+
 
 # Callbacks ***************************************************************
 @app.callback(
@@ -153,32 +131,49 @@ app.layout = html.Div(
 def update_graph(state_value, ppl_value):
     print(f"Values chosen by user: {ppl_value}")
 
+    # when no states chosen empty graph
     if len(state_value) == 0:
         return {}
-    else:
-        df_filtered = df[df["State"].isin(state_value)]
-        fig = go.Figure()
+
+    # otherwise create graph
+    df_filtered = df[df["State"].isin(state_value)]
+
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=df_filtered["State"],
+        y=df_filtered["# People Killed"],
+        name="Total People Killed",
+        offsetgroup=0,
+        hovertemplate="<br>".join([
+            "State: %{x}",
+            f"Population: {df_filtered["Population"].iloc[0]}",
+            "Total People Killed: %{y}",
+        ]),
+    ))
+
+    base = [0] * len(df_filtered["State"])
+    for chosen in ppl_value:
+
         fig.add_trace(go.Bar(
             x=df_filtered["State"],
-            y=df_filtered["# People Killed"],
-            name="Total People Killed",
+            y=df_filtered[chosen],
+            name=chosen,
+            offsetgroup=1,
+            base=base,
             hovertemplate="<br>".join([
                 "State: %{x}",
-                "Total People Killed: %{y}",
+                f"Population: {df_filtered["Population"].iloc[0]}",
+                f"{chosen}: {int(df_filtered[chosen].iloc[0])} ",
             ]),
         ))
-        for chosen in ppl_value:
-            fig.add_trace(go.Bar(
-                x=df_filtered["State"],
-                y=df_filtered[chosen],
-                name=chosen,
-                hovertemplate="<br>".join([
-                    "State: %{x}",
-                    f"{chosen}:"+" %{y}",
-                ]),
-            ))
 
-        return fig
+        base = [base_value + model_value for base_value, model_value in zip(base, df_filtered[chosen])]
+
+    fig.update_layout(
+        plot_bgcolor = '#d9e3f1',
+        paper_bgcolor = '#d9e3f1'
+    )
+    return fig
 
 if __name__ == "__main__":
     app.run_server(debug=True)
